b126b5d6ef7695cf5e8e9d63af773c2afeeae4e9
yuri-italo <yuri.italo94@gmail.com>
2024-09-22 10:37:18 -0300

    feat: add operate function

--- script.js
+++ script.js
@@ -17,3 +17,22 @@ function multiply(num, num2) {
 function divide(num, num2) {
   return num / num2;
 }
+
+function operate(operator, num, num2) {
+  switch (operator) {
+    case "+":
+      return add(num, num2);
+      break;
+    case "-":
+      return subtract(num, num2);
+      break;
+    case "*":
+      return multiply(num, num2);
+      break;
+    case "/":
+      return divide(num, num2);
+      break;
+    default:
+      break;
+  }
+}
