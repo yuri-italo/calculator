badcc995aba598b8d0e3bb655108bee84a5a47af
yuri-italo <yuri.italo94@gmail.com>
2024-09-22 10:23:05 -0300

    feat: add basic math functions

---
+++ script.js
@@ -0,0 +1,15 @@
+function add(num, num2) {
+  return +num + num2;
+}
+
+function subtract(num, num2) {
+  return num - num2;
+}
+
+function multiply(num, num2) {
+  return num - num2;
+}
+
+function divide(num, num2) {
+  return num / num2;
+}
